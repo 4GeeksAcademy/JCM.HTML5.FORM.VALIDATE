be74cc0d017725a83437726d2bc878b426ff5e41
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-07-27 15:17:02 +0000

    dos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-function saludar() {
-  console.log("Prueba");
-  alert("Hola desde el Evento Click");
-}
-var captura = document.getElementById("sendbtn");
-captura.addEventListener("click", saludar);
+let Formulario = document.querySelector("#Formulario");
+Formulario.addEventListener("submit", evento => {
+  evento.preventDefault();
+  console.log("Validando");
+  let InputCardn = document.querySelector("#InputCardn");
+  let InputCardCvc = document.querySelector("#InputCardCvc");
+
+  if (InputCardn.value !== "" && InputCardCvc.value !== "") {
+    evento.target.submit();
+  } else {
+    alert("Por favor ingrese CardN y Cvc");
+  }
+});
