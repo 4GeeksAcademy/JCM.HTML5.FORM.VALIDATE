08535636fb0ec4d2104c53dfdbfad3209f118772
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-07-27 17:05:13 +0000

    4

--- src/app.js
+++ src/app.js
@@ -10,22 +10,78 @@ Formulario.addEventListener("submit", evento => {
   let inputCity = document.querySelector("#inputCity");
   let inputState = document.querySelector("#inputState");
   let inputPostalCode = document.querySelector("#inputPostalCode");
-  let mastercard = document.querySelector("#mastercard");
-  let Americancard = document.querySelector("#Americancard");
-  let Visacard = document.querySelector("#Visacard");
-  let Paypalcard = document.querySelector("#Paypalcard");
+
+  let Error1 = "";
+  let Error2 = "";
+  let Error3 = "";
+  let Error4 = "";
+  let Error5 = "";
+  let Error6 = "";
+  let Error7 = "";
+  let Error8 = "";
+
+  if (InputCardn.value !== "" && !isNaN(InputCardn.value)) {
+  } else {
+    Error1 = "Card Number";
+  }
+  if (InputCardCvc.value !== "" && !isNaN(InputCardCvc.value)) {
+  } else {
+    Error2 = "Card CVC";
+  }
+  if (inputAmount.value !== "" && !isNaN(inputAmount.value)) {
+  } else {
+    Error3 = "Amount";
+  }
+  if (inputFirstName.value !== "" && typeof inputFirstName.value === "string") {
+  } else {
+    Error4 = "First Name";
+  }
+  if (inputLastName.value !== "" && typeof inputLastName.value === "string") {
+  } else {
+    Error5 = "Last Name";
+  }
+  if (inputCity.value !== "" && typeof inputCity.value === "string") {
+  } else {
+    Error6 = "City";
+  }
+  if (inputState.value !== "" && typeof inputState.value === "string") {
+  } else {
+    Error7 = "State";
+  }
+  if (inputPostalCode.value !== "" && !isNaN(inputPostalCode.value)) {
+  } else {
+    Error8 = "Postal Code";
+  }
+
   if (
-    InputCardn.value !== "" &&
-    InputCardCvc.value !== "" &&
-    inputAmount.value !== "" &&
-    inputFirstName.value !== "" &&
-    inputLastName.value !== "" &&
-    inputCity.value !== "" &&
-    inputState.value !== "" &&
-    inputPostalCode.value !== ""
+    Error1 == "" &&
+    Error2 == "" &&
+    Error3 == "" &&
+    Error4 == "" &&
+    Error5 == "" &&
+    Error6 == "" &&
+    Error7 == "" &&
+    Error8 == ""
   ) {
     evento.target.submit();
   } else {
-    alert("Por favor ingrese CardN y Cvc");
+    alert(
+      "Please review the following: " +
+        Error1 +
+        " " +
+        Error2 +
+        " " +
+        Error3 +
+        " " +
+        Error4 +
+        " " +
+        Error5 +
+        " " +
+        Error6 +
+        " " +
+        Error7 +
+        " " +
+        Error8
+    );
   }
 });
