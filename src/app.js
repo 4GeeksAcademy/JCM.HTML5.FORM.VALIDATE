79b7e87a5c2f0fca1594811c3570f1e7454a2b09
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-07-27 15:51:57 +0000

    3

--- src/app.js
+++ src/app.js
@@ -4,8 +4,26 @@ Formulario.addEventListener("submit", evento => {
   console.log("Validando");
   let InputCardn = document.querySelector("#InputCardn");
   let InputCardCvc = document.querySelector("#InputCardCvc");
-
-  if (InputCardn.value !== "" && InputCardCvc.value !== "") {
+  let inputAmount = document.querySelector("#inputAmount");
+  let inputFirstName = document.querySelector("#inputFirstName");
+  let inputLastName = document.querySelector("#inputLastName");
+  let inputCity = document.querySelector("#inputCity");
+  let inputState = document.querySelector("#inputState");
+  let inputPostalCode = document.querySelector("#inputPostalCode");
+  let mastercard = document.querySelector("#mastercard");
+  let Americancard = document.querySelector("#Americancard");
+  let Visacard = document.querySelector("#Visacard");
+  let Paypalcard = document.querySelector("#Paypalcard");
+  if (
+    InputCardn.value !== "" &&
+    InputCardCvc.value !== "" &&
+    inputAmount.value !== "" &&
+    inputFirstName.value !== "" &&
+    inputLastName.value !== "" &&
+    inputCity.value !== "" &&
+    inputState.value !== "" &&
+    inputPostalCode.value !== ""
+  ) {
     evento.target.submit();
   } else {
     alert("Por favor ingrese CardN y Cvc");
