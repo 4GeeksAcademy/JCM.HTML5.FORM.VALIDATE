c867efadc1319d5e278f3b6d2f0148e67f33e31d
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-07-27 20:41:07 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 let Formulario = document.querySelector("#Formulario");
+
 Formulario.addEventListener("submit", evento => {
   evento.preventDefault();
   console.log("Validando");
@@ -10,6 +11,7 @@ Formulario.addEventListener("submit", evento => {
   let inputCity = document.querySelector("#inputCity");
   let inputState = document.querySelector("#inputState");
   let inputPostalCode = document.querySelector("#inputPostalCode");
+  let divalert = document.querySelector(".alert");
 
   let Error1 = "";
   let Error2 = "";
@@ -23,34 +25,42 @@ Formulario.addEventListener("submit", evento => {
   if (InputCardn.value !== "" && !isNaN(InputCardn.value)) {
   } else {
     Error1 = "Card Number";
+    InputCardn.style.backgroundColor = "#f1aeb5";
   }
   if (InputCardCvc.value !== "" && !isNaN(InputCardCvc.value)) {
   } else {
     Error2 = "Card CVC";
+    InputCardCvc.style.backgroundColor = "#f1aeb5";
   }
   if (inputAmount.value !== "" && !isNaN(inputAmount.value)) {
   } else {
     Error3 = "Amount";
+    inputAmount.style.backgroundColor = "#f1aeb5";
   }
   if (inputFirstName.value !== "" && typeof inputFirstName.value === "string") {
   } else {
     Error4 = "First Name";
+    inputFirstName.style.backgroundColor = "#f1aeb5";
   }
   if (inputLastName.value !== "" && typeof inputLastName.value === "string") {
   } else {
     Error5 = "Last Name";
+    inputLastName.style.backgroundColor = "#f1aeb5";
   }
   if (inputCity.value !== "" && typeof inputCity.value === "string") {
   } else {
     Error6 = "City";
+    inputCity.style.backgroundColor = "#f1aeb5";
   }
   if (inputState.value !== "" && typeof inputState.value === "string") {
   } else {
     Error7 = "State";
+    inputState.style.backgroundColor = "#f1aeb5";
   }
   if (inputPostalCode.value !== "" && !isNaN(inputPostalCode.value)) {
   } else {
     Error8 = "Postal Code";
+    inputPostalCode.style.backgroundColor = "#f1aeb5";
   }
 
   if (
@@ -65,23 +75,24 @@ Formulario.addEventListener("submit", evento => {
   ) {
     evento.target.submit();
   } else {
-    alert(
+    divalert.classList.remove("d-none");
+    divalert.classList.add("d-block");
+    divalert.innerHTML =
       "Please review the following: " +
-        Error1 +
-        " " +
-        Error2 +
-        " " +
-        Error3 +
-        " " +
-        Error4 +
-        " " +
-        Error5 +
-        " " +
-        Error6 +
-        " " +
-        Error7 +
-        " " +
-        Error8
-    );
+      Error1 +
+      " " +
+      Error2 +
+      " " +
+      Error3 +
+      " " +
+      Error4 +
+      " " +
+      Error5 +
+      " " +
+      Error6 +
+      " " +
+      Error7 +
+      " " +
+      Error8;
   }
 });
